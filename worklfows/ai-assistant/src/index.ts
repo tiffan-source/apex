1cbc2ad4393c23d8fb595b4b311a556f4148db60
tiffan-source <zohoungbogboanlyou@gmail.com>
2026-08-16 18:00:12 +0200

    feat: initialize ai-assistant library with configuration, build setup, and core workflow implementation

---
+++ worklfows/ai-assistant/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/workflow/send-message-to-ai.workflow';
---
+++ worklfows/ai-assistant/src/lib/workflow/send-message-to-ai.workflow.ts
@@ -0,0 +1,67 @@
+import { ObjectiveRepository } from "@org/objective"
+import { GetAllMessagesQuery, AddMessageCommand } from "@org/chat"
+import { AIProvider } from "@org/ai"
+import { IdGenerator } from "@org/chore";
+
+export type MessageInput = {
+  message: string;
+  objectiveId: string;
+  ownerId: string;
+}
+
+export type MessageOutput = {
+  id: string;
+  response: string;
+  createdAt: Date;
+}
+
+export class SendMessageToAIWorkflow {
+  constructor(
+    private readonly idGenerator: IdGenerator,
+    private readonly objectiveRepository: ObjectiveRepository,
+    private readonly messageQuery: GetAllMessagesQuery,
+    private readonly messageGateway: AddMessageCommand,
+    private readonly aiProvider: AIProvider
+  ) {}
+
+  public async execute(input: MessageInput): Promise<MessageOutput> {
+    const { message, objectiveId, ownerId } = input;
+
+    try {
+      const { conversation } = await this.messageQuery.execute({ externalReferenceId: objectiveId });
+
+      await this.messageGateway.execute({
+        id: this.idGenerator.generateId(),
+        conversationId: conversation.id,
+        message,
+        userId: ownerId
+      });
+
+      const objective = await this.objectiveRepository.findById(objectiveId);
+
+      let context = JSON.stringify({
+        objective,
+        conversation
+      });
+
+      let response = await this.aiProvider.generateMessage(`You are an AI assistant. You will receive a message from a user and you will respond to it. The context of the conversation is: ${context}. The user's message is: ${message}. Please provide a helpful and relevant response.`)
+
+      let resultAi = await this.messageGateway.execute({
+        id: this.idGenerator.generateId(),
+        conversationId: conversation.id,
+        message: response,
+        userId: "ai-assistant"
+      });
+
+      return {
+        id: resultAi.id,
+        response: resultAi.message,
+        createdAt: resultAi.createdAt
+      };
+    } catch (error) {
+      console.error("Error occurred while sending message to AI:", error);
+      throw new Error("Failed to send message to AI. Please try again later.");
+    }
+  }
+
+}
